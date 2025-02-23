cb562c5723b97c3554d6cb4ba6811ddff7b5fe82
Rohit Karthick <rkbeo5@gmail.com>
2025-02-23 12:03:10 +0530

    renamed

---
+++ Basic Python/00_All_Exercises.py
@@ -0,0 +1,15 @@
+# ## Exercise: Python Variables
+
+# **Q1. Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables**
+# birth_year = 2001
+# current_year = 2024
+
+# age = current_year - birth_year 
+# print( "My age:",age)
+
+# **Q2. Store your first, middle and last name in three different variables and then print your full name using these variables**
+# first_name = "Rohit"
+# last_name = "Karthick"
+
+# full_name = first_name + " " + last_name
+# print("Full Name:",full_name)
